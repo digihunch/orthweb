a379fd3479fefa8257e887cd02c59affb926f8c1
DigiHunch <y_lu@live.com>
2021-05-14 14:57:47 -0400

    add aws secret store to store param

--- terraform/database.tf
+++ terraform/database.tf
@@ -1,3 +1,12 @@
+data "aws_secretsmanager_secret_version" "creds" {
+  secret_id = "db-creds"
+}
+
+locals {
+  db_creds = jdondecode(
+    data.aws_secretsmanager_secret_version.creds.secret_string
+  )
+}
 
 resource "aws_db_instance" "postgres" {
   allocated_storage         = 5
@@ -5,10 +14,12 @@ resource "aws_db_instance" "postgres" {
   engine                    = "postgres"
   engine_version            = "12.2"
   instance_class            = "db.t2.micro"
-  identifier                = "orthandpostgres"
+  identifier                = "orthancpostgres"
   name                      = "orthancdb"
-  username                  = "myuser"
-  password                  = "mpassword"
+#  username                  = "myuser"
+#  password                  = "mpassword"
+  username                  = local.db_creds.username
+  password                  = local.db_creds.password
   port                      = "5432"
   deletion_protection       = false
   skip_final_snapshot       = "true"
