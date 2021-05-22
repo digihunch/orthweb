6e7b6c4fc0a67185264d309e457d87c48fb00edc
DigiHunch <y_lu@live.com>
2021-05-22 17:52:00 -0400

    enable encryption at rest on postgres and update instance type.

--- terraform/database.tf
+++ terraform/database.tf
@@ -1,10 +1,14 @@
+resource "aws_kms_key" "dbkey" {
+  description             = "This key is used to encrypt database storage"
+  deletion_window_in_days = 10
+}
 
 resource "aws_db_instance" "postgres" {
   allocated_storage                   = 5
   storage_type                        = "standard" #magnetic drive minimum 5g storage
   engine                              = "postgres"
   engine_version                      = "12.2"
-  instance_class                      = "db.t2.micro"
+  instance_class                      = "db.t2.small" # t2.micro does not support encryption at rest
   identifier                          = "orthancpostgres"
   name                                = "orthancdb"
   username                            = local.db_creds.username
@@ -16,5 +20,7 @@ resource "aws_db_instance" "postgres" {
   final_snapshot_identifier           = "demodb"
   vpc_security_group_ids              = ["${aws_security_group.dbsecgroup.id}"]
   db_subnet_group_name                = aws_db_subnet_group.default.name
+  storage_encrypted                   = true
+  kms_key_id                          = aws_kms_key.dbkey.arn
 }
 
