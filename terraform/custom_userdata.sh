a7a16203eb1500a330d8f8a238ef48da4e6fec34
DigiHunch <y_lu@live.com>
2021-05-26 10:35:44 -0400

    rename directory docker to app

--- terraform/custom_userdata.sh
+++ terraform/custom_userdata.sh
@@ -7,16 +7,16 @@ runuser -l ec2-user -c "
   cd /home/ec2-user/ && git init orthweb && cd orthweb && \
   git remote add origin https://github.com/digihunch/orthweb.git && \
   git config core.sparsecheckout true && \
-  echo 'docker/*' >> .git/info/sparse-checkout && \
+  echo 'app/*' >> .git/info/sparse-checkout && \
   git pull --depth=1 origin main 
 "
-cd /home/ec2-user/orthweb/docker
+cd /home/ec2-user/orthweb/app
 cat /tmp/private.key /tmp/certificate.crt > $ComName.pem && rm /tmp/private.key /tmp/certificate.crt
 chown ec2-user:ec2-user $ComName.pem
 sed -i -e 's/sample\.localhost\.pem/'$ComName'.pem/g' docker-compose.yml
 
 runuser -l ec2-user -c "
-  cd /home/ec2-user/orthweb/docker && docker-compose up
+  cd /home/ec2-user/orthweb/app && docker-compose up
 "
 
 echo "Leaving custom script"
